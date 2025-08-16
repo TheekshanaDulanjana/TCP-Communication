8eddb64792a9c21b24b2548dc28916d2d0af3d82
OPT Dulanjana <140092772+TheekshanaDulanjana@users.noreply.github.com>
2025-08-16 22:10:03 +0530

    First Commit

---
+++ Assignment01/TcpClientApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System. Text;
+
+Console.WriteLine("Connecting to server ... ");
+
+TcpClient client = new TcpClient();
+await client.ConnectAsync("127.0.0.1", 5000);
+
+NetworkStream stream = client.GetStream();
+
+string message = "Hello from client!";
+byte[] data = Encoding.UTF8.GetBytes(message);
+await stream.WriteAsync(data, 0, data.Length);
+
+byte[] buffer = new byte[1024];
+int bytesRead = await stream. ReadAsync(buffer, 0, buffer. Length);
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+Console.WriteLine("Server replied: " + response);
+
+Console.WriteLine("Press ENTER to close connection.");
+Console.ReadLine();
+
+client.Close();
---
+++ Assignment01/TcpServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServerApp
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var ip = IPAddress.Any;
+            var listener = new TcpListener(ip, 5000);
+            listener.Start();
+            Console.WriteLine("Server started. Waiting for connection ... " + ip);
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected!");
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer);
+                string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received from client: {received}");
+
+                string response = $"Hello, client! You said: {received}";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes);
+
+                client.Close();
+            }
+        }
+    }
+}
